49ba8460f7f639568eec28a7b6dcbf6ac9ca4be0
Todd Cope <xander4jesus@yahoo.com>
2015-11-04 22:04:52 -0600

    Only require size argument for destination buffer.

--- include/t3f/ustring.h
+++ include/t3f/ustring.h
@@ -1,14 +1,14 @@
 #ifndef T3F_STRING_H
 #define T3F_STRING_H
 
-int t3f_strget(const char * s, int point, int size);
+int t3f_strget(const char * s, int point);
 int t3f_strset(char * s, int point, int val, int size);
-int t3f_strcasecmp(const char *, int size1, const char *, int size2);
-int t3f_strcmp(const char *, int size1, const char *, int size2);
+int t3f_strcasecmp(const char *, const char *);
+int t3f_strcmp(const char *, const char *);
 char * t3f_strcat(char *, const char *, int size);
 char * t3f_strcpy(char *, const char *, int size);
 char * t3f_strdup(const char * s1, int size);
-size_t t3f_strlen(const char *, int size);
+size_t t3f_strlen(const char *);
 char * t3f_strupr(char *, int size);
 char * t3f_strlower(char *, int size);
 
